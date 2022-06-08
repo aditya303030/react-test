9f045b646b9666ea49bdf94eb5dea5fb578c55fd
aditya303030 <73743156+aditya303030@users.noreply.github.com>
2022-06-09 01:28:29 +0530

    home component changes

--- src/components/home.js
+++ src/components/home.js
@@ -1,12 +1,21 @@
-import React from "react";
+import React,{useState} from "react";
 import '../App.css';
 import Navbar from "./home components/navbar";
 
 const Home = () => {
+  //states
+  const [Text,setText] = useState('');
+  const InputTextHandler = (e) => {
+    setText(e.target.value)
+    console.log(e.target.value)
+  }
   return (
     <div className="container">
       <Navbar />
       <h1>Home Page</h1>
+      <input placeholder="input text" ></input>
+      <button type="submit" onclick={InputTextHandler}>Submit</button>
+      <h1>{Text}</h1>
     </div>
   );
 };
