220e79837aa04cc985c23d067cce21600f197399
aditya303030 <73743156+aditya303030@users.noreply.github.com>
2022-07-01 18:04:35 +0530

    added a todo file and completed the todo-list app

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
 import React,{useReducer, useState} from "react";
 import './App.css';
+import Todo from "./todo";
 
 const ACTIONS = {
-  NEW_TODO: 'new_todo'
+  NEW_TODO: 'new_todo',
+  TOGGLE_TODO: 'toggle_todo',
+  DELETE_TODO:'delete_todo'
 }
+export {ACTIONS}
 
 const reducer = (todos,action) => {
   switch(action.type){
     case ACTIONS.NEW_TODO:
       return [...todos,newTodos(action.payload.name)]
+    case ACTIONS.TOGGLE_TODO:
+      return todos.map(todo=> {
+        if (todo.id===action.payload.id) {
+          return{...todo,complete:!todo.complete}
+        }
+        return todo; 
+      })
+    case ACTIONS.DELETE_TODO:
+      return todos.filter(todo=> todo.id !== action.payload.id)
     default:
       return todos
   }
@@ -33,12 +46,10 @@ function App() {
     <>
       <form onSubmit={HandleSubmit}>
         <input type="text" value={name} onChange={e => setName(e.target.value)}></input>
-        <ul>
-          {todos.map((key,value)=> {
-            return <li key="{todos.name}">{todos.name}</li>
-          })}
-        </ul>
       </form>
+      {todos.map(todo => {
+        return <Todo key = {todo.id} todo={todo} dispatch = {dispatch} />
+      })}
     </>
   )
 }
